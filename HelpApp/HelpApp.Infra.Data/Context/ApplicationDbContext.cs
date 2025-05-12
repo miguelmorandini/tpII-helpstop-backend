c1fed9c4d955c5cd8525dcb56f8d695684508cb8
Miguel Morandini <miguelmorandini2005@gmail.com>
2025-05-12 10:54:19 -0300

    Correção da Avaliação1

---
+++ HelpApp/HelpApp.Infra.Data/Context/ApplicationDbContext.cs
@@ -0,0 +1,21 @@
+﻿using HelpApp.Domain.Entities;
+using Microsoft.EntityFrameworkCore;
+using System.Collections.Generic;
+using System.Reflection.Emit;
+
+namespace HelpApp.Infra.Data.Context
+{
+    public class ApplicationDbContext : DbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options) { }
+        public DbSet<Category> Categories { get; set; }
+        public DbSet<Product> Products { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder builder)
+        {
+            base.OnModelCreating(builder);
+            builder.ApplyConfigurationsFromAssembly(typeof(ApplicationDbContext).Assembly);
+
+        }
+    }
+}
