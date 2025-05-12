f9e1cdae34f70012598ca8c35677bc029884923f
Miguel Morandini <miguelmorandini2005@gmail.com>
2025-05-12 12:46:35 -0300

    Implemetação de Interface de Category

---
+++ HelpApp/HelpApp.Application/Interfaces/ICategoryService.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections.Generic;
+using System.Threading.Tasks;
+using HelpApp.Application.DTOs;
+
+namespace HelpApp.Application.Interfaces
+{
+    public interface ICategoryService
+    {
+        Task<IEnumerable<CategoryDTO>>GetCategories();
+        Task<CategoryDTO>GetById(int? id);
+        Task Add (CategoryDTO categoryDTO);
+        Task Update (CategoryDTO categoryDTO);  
+        Task Remove (int? id);
+
+    }
+}
