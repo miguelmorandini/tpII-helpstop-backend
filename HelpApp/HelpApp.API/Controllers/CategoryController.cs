b46da36d022965e5903b75f77444c885a87e09db
Miguel Morandini <miguelmorandini2005@gmail.com>
2025-05-19 12:43:05 -0300

    Modelo MediatR e Command de Produtos

---
+++ HelpApp/HelpApp.API/Controllers/CategoryController.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace HelpApp.API.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class CategoryController : ControllerBase
+    {
+    }
+}
